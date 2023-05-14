8af304f4c8950f6d89cffa8fcb3d1ac895df98a6
jgo-42 <jingmuio@gmail.com>
2023-05-14 11:27:11 +0900

    :bento: Feat: Rules.mk

---
+++ Rules.mk
@@ -0,0 +1,50 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Rules.mk                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/11/05 14:02:20 by jgo               #+#    #+#              #
+#    Updated: 2023/05/14 11:24:16 by jgo              ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+.DEFAULT_GOAL := all
+
+PROJECT_NAME := 42curus
+ARFLAGS := rcs
+CFLAGS := -Wall -Wextra -Werror -MMD -MP
+
+# verbose
+Q := $(if $(filter 1,$(V) $(VERBOSE)),,@)
+
+# debug
+ifdef DEBUG
+	CFLAGS += -g3
+endif
+
+# just compile
+ifdef JUST
+	CFLAGS := -MMD -MP
+endif
+
+# address
+ifdef ADDR
+	CFLAGS += -fsanitize=address
+endif
+
+ifdef RACE
+	CFLAGS := -fsanitize=thread -MMD -MP -g3
+	LDFLAGS += -fsanitize=thread -g3
+endif
+
+link_files:: unlink_files
+	$(Q)$(call color_printf,$(GRAY),includes,📁 make includes folder)
+	mkdir -p $(dst_dir);
+	$(Q)$(foreach file,$(files), $(call color_printf,$(CYAN),$(file),🔗 linking file\n) ln -sf $(src_dir)/$(file) $(dst_dir);)
+	$(Q)$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir);)
+
+unlink_files::
+	$(Q)$(foreach file,$(files), $(call color_printf,$(GRAY),$(file),🚫 unlinking file\n) $(RM) $(dst_dir)/$(file);)
+	$(Q)$(foreach file,$(files), $(RM) $(dst_dir)/$(file);)
