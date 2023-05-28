02a540b216948e21e2fb734bf27ad26246ca6e3f
jgo-42 <jingmuio@gmail.com>
2023-05-28 16:58:16 +0900

    :building_construction: Fix: lib compile receipe

--- src/Recipes.mk
+++ src/Recipes.mk
@@ -6,12 +6,15 @@
 #    By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/24 20:22:27 by jgo               #+#    #+#              #
-#    Updated: 2023/05/25 16:43:53 by jgo              ###   ########.fr        #
+#    Updated: 2023/05/28 16:52:25 by jgo              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+$(DIRS):
+	$(MAKE) -C $@
+
 all bonus :
-	$(Q)$(foreach dir, $(DIRS), $(MAKE) TOPDIR=$(TOPDIR) -C $(dir) $@;)
+	$(Q)$(MAKE) $(DIRS)
 	$(Q)$(call color_printf,$(YELLOW),$(NAME),🎯 Start compiling)
 	$(MAKE) $(NAME)
 	$(Q)$(call color_printf,$(GREEN),$(NAME),✨ compiled!)
@@ -35,4 +38,4 @@ re: fclean
 	$(Q)$(foreach dir, $(DIRS), $(MAKE) TOPDIR=$(TOPDIR) -C $(dir) $@;)
 	$(MAKE)
 
-.PHONY: all bonus clean fclean re
+.PHONY: all bonus clean fclean re $(DIRS)
