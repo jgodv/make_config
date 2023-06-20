e4a7bebbae2e4eaa3e86a03708b8de0e3b651c39
jgo-42 <jingmuio@gmail.com>
2023-06-20 17:27:19 +0900

    :rotating_light: Fix(bonus): fix a compile rules

--- src/BonusSubLibsRecipes.mk
+++ src/BonusSubLibsRecipes.mk
@@ -6,20 +6,17 @@
 #    By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/24 20:28:04 by jgo               #+#    #+#              #
-#    Updated: 2023/06/19 09:53:56 by jgo              ###   ########.fr        #
+#    Updated: 2023/06/20 17:21:07 by jgo              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-all bonus : 
+all bonus : $(OBJS)
 	$(Q)$(call color_printf,$(CYAN),$(NAME),🎯 starting compile $(NAME))
-	$(Q)$(MAKE) $(NAME)
+	$(Q)$(call color_printf,$(GREEN),$(NAME),📚 archive object)
+	$(AR) $(ARFLAGS) $(NAME) $^
+	$(Q)$(MAKE) files="$(NAME)" src_dir=`pwd` dst_dir=$(TOPDIR)/$(BONUS_LIB_DIR) link_files
 	$(Q)$(call color_printf,$(GREEN),$(NAME),🔰 done!)
 
-$(NAME): $(OBJS)
-	$(Q)$(call color_printf,$(GREEN),$(NAME),📚 archive object)
-	$(AR) $(ARFLAGS) $@ $^
-	$(Q)$(MAKE) files="$(NAME)" src_dir=`pwd` dst_dir=$(TOPDIR)/$(BONUS_LIB_DIR) link_files
-	
 clean:
 	$(Q)$(MAKE) files="$(NAME)" src_dir=`pwd` dst_dir=$(TOPDIR)/$(BONUS_LIB_DIR) unlink_files
 	$(Q)$(call color_printf,$(RED),$(NAME),🗑️  remove Objects && Dependency file)
@@ -28,7 +25,7 @@ clean:
 fclean: clean
 	$(Q)$(call color_printf,$(RED),$(NAME),🗑️  remove $(NAME))
 	$(RM) $(NAME)
-	
+
 re : fclean
 	$(MAKE) all
 
