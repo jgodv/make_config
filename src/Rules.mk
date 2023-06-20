701173c85f5a97963b14dedf4a6aefff67f13a4a
jgo-42 <jingmuio@gmail.com>
2023-06-20 17:00:44 +0900

    :truck:  Fix(bonus): bonus name change

--- src/Rules.mk
+++ src/Rules.mk
@@ -6,13 +6,14 @@
 #    By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/05 14:02:20 by jgo               #+#    #+#              #
-#    Updated: 2023/06/19 09:55:49 by jgo              ###   ########.fr        #
+#    Updated: 2023/06/20 17:00:07 by jgo              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 .DEFAULT_GOAL := all
 
 PROJECT_NAME := miniRT
+BONUS_NAME := miniRT_bonus
 
 MANDATORY_DIR := mandatory
 BONUS_DIR := bonus
@@ -22,7 +23,7 @@ BONUS_LIB_DIR := src/$(BONUS_DIR)/lib
 
 ARFLAGS := rcs
 CFLAGS := -O3 -Wall -Wextra -Werror -MMD -MP
-CPPFLAGS = -I$(TOPDIR)/includes -I$(TOPDIR)/ext/MLX42/include/MLX42 $(if $(filter bonus, $(MAKECMDGOALS)), -I$(TOPDIR)/src/$(BONUS_DIR)/includes, -I$(TOPDIR)/src/$(MANDATORY_DIR)/includes)
+CPPFLAGS := -I$(TOPDIR)/includes -I$(TOPDIR)/ext/MLX42/include/MLX42 $(if $(findstring bonus, $(MAKECMDGOALS)), -I$(TOPDIR)/src/$(BONUS_DIR)/includes, -I$(TOPDIR)/src/$(MANDATORY_DIR)/includes)
 LDFLAGS := -L$(TOPDIR)/lib -L$(TOPDIR)/ext -L$(shell brew --prefix glfw)/lib
 LDLIBS := -lft_arr -lft_ascii -lft_io -lft_is -lft_lst -lft_math -lft_mem -lft_str -lvector -langle -lequation -lglfw -lmlx42 -framework Cocoa -framework OpenGL -framework IOKit
 
