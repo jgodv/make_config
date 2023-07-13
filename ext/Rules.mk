639fa255c1988eabb0e2cb77d34c2b20586c1842
JinMu Go <jingmuio@gmail.com>
2023-07-13 20:30:50 +0900

    :pushpin: Fix(src): mms mlx

--- ext/Rules.mk
+++ ext/Rules.mk
@@ -6,16 +6,14 @@
 #    By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/06 17:38:45 by jgo               #+#    #+#              #
-#    Updated: 2023/06/06 18:02:16 by jgo              ###   ########.fr        #
+#    Updated: 2023/07/13 10:49:29 by jgo              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 .DEFAULT_GOAL := all
 
-PROJECT_NAME := MLX42
-ARCHIVE_NAME := libmlx42.a
-
-RM += -r
+PROJECT_NAME := mlx_opengl
+ARCHIVE_NAME := libmlx.a
 
 BLD_DIR := $(PROJECT_NAME)/build
 MLX_LIB :=
@@ -45,7 +43,7 @@ endif
 
 link_files:: unlink_files
 	$(Q)$(call color_printf,$(GRAY),includes,📁 make includes folder)
-	mkdir -p $(dst_dir);
+	mkdir -p $(dst_dir)
 	$(Q)$(foreach file,$(files), $(call color_printf,$(CYAN),$(file),🔗 linking file\n) ln -sf $(src_dir)/$(file) $(dst_dir);)
 	$(Q)$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir);)
 
